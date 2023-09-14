1cb031e9fdfae7448aa4ce1615137907d74af18d
Roland Hesse <rolandhesse11783@gmail.com>
2023-09-14 10:09:11 +0200

    implement suggestions for PR4

--- components/ArtPieceDetails/index.js
+++ components/ArtPieceDetails/index.js
@@ -7,15 +7,14 @@ export default function ArtPieceDetails({ image, name, artist, year, genre }) {
     router.push("/art-pieces");
   }
   return (
-    <>
-      <section>
-        <button onClick={handleBack}>Back</button>
-        <Image width={200} height={200} alt="test" src={image} />
-        <br />
-        <small>{`"${name}" by ${artist}`}</small>
-        <small>Year : {year}</small>
-        <small>genre : {genre}</small>
-      </section>
-    </>
+    <section>
+      <button onClick={handleBack}>Back</button>
+      <br />
+      <Image width={200} height={200} alt={name} src={image} />
+      <br />
+      <h3>{`"${name}" by ${artist}`}</h3>
+      <small>Year : {year}</small>
+      <small>genre : {genre}</small>
+    </section>
   );
 }
